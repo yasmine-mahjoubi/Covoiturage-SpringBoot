4ba24b6cd87272f37f46da5b1a775ccbfc96326a
yasmine-mahjoubi <yassminemahjoubi764@gmail.com>
2024-12-11 22:55:39 +0100

    L'ajout de l'implementation de l'interface ReservationService

---
+++ src/main/java/com/fst/projet_CarPooling_jee/Service/impl/ReservationServiceImpl.java
@@ -0,0 +1,58 @@
+package com.fst.projet_CarPooling_jee.Service.impl;
+
+import com.fst.projet_CarPooling_jee.Entity.Reservation;
+import com.fst.projet_CarPooling_jee.Entity.enums.ReservationStatus;
+import com.fst.projet_CarPooling_jee.Repository.ReservationRepository;
+import com.fst.projet_CarPooling_jee.Service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Override
+    public Reservation createReservation(Reservation reservation) {
+        return reservationRepository.save(reservation);
+    }
+
+    @Override
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    @Override
+    public Reservation getReservationById(Long id) {
+        return reservationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Reservation not found with ID: " + id));
+    }
+
+    @Override
+    public Reservation updateReservation(Long id, Reservation updatedReservation) {
+        Reservation existingReservation = getReservationById(id);
+
+        existingReservation.setNbReservedPlaces(updatedReservation.getNbReservedPlaces());
+        existingReservation.setStatus(updatedReservation.getStatus());
+        existingReservation.setUser(updatedReservation.getUser());
+        existingReservation.setRide(updatedReservation.getRide());
+
+        return reservationRepository.save(existingReservation);
+    }
+
+    @Override
+    public void deleteReservation(Long id) {
+        Reservation reservation = getReservationById(id);
+        reservationRepository.delete(reservation);
+    }
+
+    @Override
+    public Reservation updateReservationStatus(Long id, ReservationStatus status) {
+        Reservation reservation = getReservationById(id);
+        reservation.setStatus(status);
+        return reservationRepository.save(reservation);
+    }
+}
